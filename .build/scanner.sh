0aaa53154a3a5c403092578d464c53c9d5f4d107
Marcel <marcel@mahillmann.de>
2020-04-07 13:34:14 +0200

    Jenkinsfile
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- .build/scanner.sh
+++ .build/scanner.sh
@@ -2,7 +2,7 @@
 [ -d reports ] && rm -rf reports
 mkdir reports
 [ ! -f .coverage ] && rm -rf .coverage
-python3 --version
+python3.7 --version
 exit 99
 pip3 install --user -r requirements.txt > /dev/null 2>&1
 nosetests -c .build/nose.cfg
--- openssl_test.py
+++
@@ -1,8 +0,0 @@
-import ssl
-
-if __name__ == "__main__":
-    for t in ssl.TLSVersion:
-        print("1: ", t)
-
-    print(ssl.TLSVersion.MINIMUM_SUPPORTED)
-    print(ssl.TLSVersion.MAXIMUM_SUPPORTED)
