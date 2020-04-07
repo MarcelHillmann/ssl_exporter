e2ed94ae000658deefd0aca3bb105815f8f4b3e4
Marcel <marcel@mahillmann.de>
2020-04-07 11:00:02 +0200

    Jenkinsfile
    Signed-off-by: Marcel <marcel@mahillmann.de>

--- .build/scanner.sh
+++ .build/scanner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[ ! -d reports ] && mkdir reports &&\
-pip3 install --user -r requirements.txt &&\
-nosetests -c .build/nose.cfg &&\
+[ ! -d reports ] && mkdir reports
+pip3 install --user -r requirements.txt
+nosetests -c .build/nose.cfg
 sonar-scanner -X
 ls -AlhX
